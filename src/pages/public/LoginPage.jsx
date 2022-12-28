fdf42f42f93221fe2edb923631ca2987f63e9e21
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-12-28 22:14:41 +0100

    add loader

--- src/pages/public/LoginPage.jsx
+++ src/pages/public/LoginPage.jsx
@@ -9,6 +9,7 @@ function LoginPage({ isConnected, setIsConnected }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showAlert, setShowAlert] = useState(false);
+  const [loader, setLoader] = useState(false);
   const navigate = useNavigate();
 
   const handleChangeEmail = (event) => {
@@ -20,6 +21,7 @@ function LoginPage({ isConnected, setIsConnected }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoader(true);
 
     axios
       .post("https://test-technique-strateg-in.vercel.app/api/login", {
@@ -33,10 +35,12 @@ function LoginPage({ isConnected, setIsConnected }) {
       .then((res) => {
         setShowAlert(false);
         navigate("/users");
+        setLoader(false);
       })
       .catch((error) => {
         console.error(error);
         setShowAlert(true);
+        setLoader(false);
       });
   };
 
@@ -97,7 +101,7 @@ function LoginPage({ isConnected, setIsConnected }) {
             type="submit"
             className="transition duration-150 ease-out hover:ease-in  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            {isConnected ? (
+            {loader ? (
               <svg
                 aria-hidden="true"
                 role="status"
